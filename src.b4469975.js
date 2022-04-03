e5a66835c71349a728ff78e0d799e47688fbbbde
ArkZaslavskyi <95150973+ArkZaslavskyi@users.noreply.github.com>
2022-04-03 12:22:04 +0000

    Deploying to gh-pages from @ ArkZaslavskyi/ice-cream-project-1@046fa4571ebfb951b8f7b9c4e0dc1d44e176ed57 🚀

--- src.b4469975.js
+++ src.b4469975.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/ice-cream-project-1/src.f34b47b5.js.map
+//# sourceMappingURL=/ice-cream-project-1/src.b4469975.js.map
